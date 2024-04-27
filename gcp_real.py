5e856aa37dc1d17f9f42f69049c037c805098af4
Alireza Dizaji <alirezadizaji@yahoo.com>
2024-04-27 16:22:34 -0400

    Update: running config

--- gcp_real.py
+++ gcp_real.py
@@ -6,32 +6,42 @@ import jax
 from sys import argv
 
 import loss_functions as L
-from gcp import decompose_lbfgs, init_cp
+import gcp as G
 from data_preprocessing import process_ocn
 
 if __name__ == '__main__':
     T = process_ocn(argv[1], num_active_users=200)
+    configs = {
+        "init": ["init_cp", "init_tt"],
+        "dist": ['normal', 'poisson_log', 'bernoulli_logit'],
+        "objective_fun": ["cp", "tt"],
+        "r": [2, 5, 10],
+        "mask_ratio": [0.1, 0.5, 0.9],
+    }
     seed = 0
     N = 3
     d = jnp.array(T.shape)
     assert N == len(d)
-    r = 20
-
     num_iters = 1000
-    loss = np.zeros(num_iters)
-    lr = 0.01
 
-    # Clear the JIT cache and choose the loss function
-    dist = 'normal'
-    jax.clear_caches()
-    L.loss_fun = getattr(L, dist)
 
-    # Generate mask
-    mask = 1 - rnd.binomial(n=1, p=0.1, size=d).astype(np.float32)
+    for init_func_name in configs["init"]:
+        for  dist in configs["dist"]:
+            for  obj_fun in configs["objective_fun"]:
+                for r in configs["r"]:
+                    for  mask_ratio in configs["mask_ratio"]:
+                        loss = np.zeros(num_iters)
+                        lr = 0.01
+                    
+                        # Generate mask
+                        mask = 1 - rnd.binomial(n=1, p=mask_ratio, size=d).astype(np.float32)
 
-    # r = jnp.array([2, 6, 20, 5])  # Full-rank
-    # r = jnp.array([2, 2, 2, 2])  # Low-rank
-    r = 2
-    U0 = init_cp(d, r, seed=seed)
-    decompose_lbfgs(T, mask, U0, objective_fun='cp')
-    print()
+                        # Clear the JIT cache and choose the loss function
+                        dist = 'normal'
+                        jax.clear_caches()
+                        L.loss_fun = getattr(L, dist)
+
+                        init_fun = getattr(G, init_func_name)
+                        U0 = init_fun(d, r, seed=seed)
+                        U, loss_mask, loss_full = G.decompose_lbfgs(T, mask, U0, objective_fun=obj_fun)
+                        print(f"*** init_fun: {init_func_name}, mask_ratio: {mask_ratio}, dist: {dist}, obj_fun: {obj_fun}, r: {r} -> loss_mask: {loss_mask}, loss_full: {loss_full}",  flush=True)
