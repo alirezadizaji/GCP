6e5f5d579b00aebaafa74a51d941c8cedcb09ba2
Alireza Dizaji <alirezadizaji@yahoo.com>
2024-04-29 15:03:55 -0400

    Update: replace sys with argumentparser

--- gcp_real.py
+++ gcp_real.py
@@ -1,47 +1,78 @@
+
+from argparse import ArgumentParser
+import os
+from sys import argv
+
 import jax.numpy as jnp
 import numpy as np
 import numpy.random as rnd
 import jax
-
-from sys import argv
 
 import loss_functions as L
 import gcp as G
 from data_preprocessing import process_ocn
 
 if __name__ == '__main__':
-    T = process_ocn(argv[1], num_active_users=200)
-    configs = {
-        "init": ["init_cp", "init_tt"],
-        "dist": ['normal', 'poisson_log', 'bernoulli_logit'],
-        "objective_fun": ["cp", "tt"],
-        "r": [2, 5, 10],
-        "mask_ratio": [0.1, 0.5, 0.9],
-    }
+    parser = ArgumentParser(description='Data completion task using GCP and GTT on OCN dataset.')
+    parser.add_argument('-f', '--filedir', required=True, type=str, help='OCN file directory.')
+    parser.add_argument('-r', '--rank', type=int, nargs="+", help='Factorization rank.')
+    parser.add_argument('-m', '--mask-ratio', type=float, nargs="+", help='Mask ratios to apply on the input to run data completion task.')
+    parser.add_argument('-j', '--obj-func', type=str, nargs="+")
+    parser.add_argument('-d', '--dist', type=str, nargs="+")
+    parser.add_argument('-s', '--save-dir', type=str, required=True)
+    args = parser.parse_args()
+    print("## Args ##")
+    print(' '.join(f'{k}={v}' for k, v in vars(args).items()))
+    
+    T = process_ocn(args.filedir, num_active_users=200)
+
+    # configs = {
+    #     "dist": ['normal', 'poisson_log', 'bernoulli_logit'],
+    #     "objective_fun": ["cp", "tt"],
+    #     "r": [2, 5, 10],
+    #     "mask_ratio": [0.1, 0.5, 0.9],
+    # }
+
     seed = 0
     N = 3
     d = jnp.array(T.shape)
     assert N == len(d)
     num_iters = 1000
 
+    lr = 0.01
 
-    for init_func_name in configs["init"]:
-        for  dist in configs["dist"]:
-            for  obj_fun in configs["objective_fun"]:
-                for r in configs["r"]:
-                    for  mask_ratio in configs["mask_ratio"]:
-                        loss = np.zeros(num_iters)
-                        lr = 0.01
+    os.makedirs(args.save_dir, exist_ok=True)
+    
+    for obj_fun in args.obj_func:
+        for dist in args.dist:
+            for r in args.rank:
+                
+                r = int(r)
+                if obj_fun == "tt":
+                    r = [r] * (N - 1)    
+
+                for mask_ratio in args.mask_ratio:
+                    init_func = f"init_{obj_fun}"
+                    filename = f"{obj_fun}_{dist}_{r}_{mask_ratio}.npz"
+                    sd = os.path.join(args.save_dir, filename)
                     
-                        # Generate mask
-                        mask = 1 - rnd.binomial(n=1, p=mask_ratio, size=d).astype(np.float32)
+                    print(f"** Start running init_fun: {init_func}, mask_ratio: {mask_ratio}, dist: {dist}, obj_fun: {obj_fun}, r: {r} ...", flush=True)
+                    if os.path.exists(sd):
+                        print(f"\tSkip; Done before.")
+                        continue
+                    
+                    loss = np.zeros(num_iters)
+                
+                    # Generate mask
+                    mask = 1 - rnd.binomial(n=1, p=float(mask_ratio), size=d).astype(np.float32)
 
-                        # Clear the JIT cache and choose the loss function
-                        dist = 'normal'
-                        jax.clear_caches()
-                        L.loss_fun = getattr(L, dist)
+                    # Clear the JIT cache and choose the loss function
+                    jax.clear_caches()
+                    L.loss_fun = getattr(L, dist)
 
-                        init_fun = getattr(G, init_func_name)
-                        U0 = init_fun(d, r, seed=seed)
-                        U, loss_mask, loss_full = G.decompose_lbfgs(T, mask, U0, objective_fun=obj_fun)
-                        print(f"*** init_fun: {init_func_name}, mask_ratio: {mask_ratio}, dist: {dist}, obj_fun: {obj_fun}, r: {r} -> loss_mask: {loss_mask}, loss_full: {loss_full}",  flush=True)
+                    init_fun = getattr(G, init_func)
+                    U0 = init_fun(d, r, seed=seed)
+                    U, loss_mask, loss_full = G.decompose_lbfgs(T, mask, U0, objective_fun=obj_fun)
+
+                    print(f"\tDone -> loss_mask: {loss_mask}, loss_full: {loss_full}",  flush=True)
+                    np.savez_compressed(sd, loss_mask=loss_mask, loss_full=loss_full)
